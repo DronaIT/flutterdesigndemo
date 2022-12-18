242f876ec7f1404620ab6291b4726f491c6ddb3d
Gaurav Balbhadra <Gaurav Balbhadra>
2022-12-18 17:45:46 +0530

    specialization detail screen

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -6,7 +6,7 @@ import 'package:flutterdesigndemo/customwidget/custom_text.dart';
 import 'package:flutterdesigndemo/models/base_api_response.dart';
 import 'package:flutterdesigndemo/models/home_module_response.dart';
 import 'package:flutterdesigndemo/ui/login.dart';
-import 'package:flutterdesigndemo/ui/manage_specializations.dart';
+import 'package:flutterdesigndemo/ui/academic_details.dart';
 import 'package:flutterdesigndemo/ui/manage_user.dart';
 import 'package:flutterdesigndemo/ui/setting.dart';
 import 'package:flutterdesigndemo/utils/preference.dart';
@@ -151,7 +151,7 @@ class _HomeState extends State<Home> {
                                     onTap: () {
                                       if (homeModule.records![index].fields?.moduleId == "DM01")
                                         Get.to(ManageUser());
-                                      else if (homeModule.records![index].fields?.moduleId == "DM05") Get.to(ManageSpecializations());
+                                      else if (homeModule.records![index].fields?.moduleId == "DM05") Get.to(AcademicDetails());
                                     },
                                   ),
                                 ),
--- lib/ui/specialization_detail.dart
+++ lib/ui/specialization_detail.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutterdesigndemo/api/api_repository.dart';
+import 'package:flutterdesigndemo/api/service_locator.dart';
+import 'package:flutterdesigndemo/customwidget/app_widgets.dart';
+import 'package:flutterdesigndemo/customwidget/custom_text.dart';
+import 'package:flutterdesigndemo/models/base_api_response.dart';
+import 'package:flutterdesigndemo/models/specialization_response.dart';
+import 'package:flutterdesigndemo/utils/tablenames.dart';
+import 'package:flutterdesigndemo/utils/utils.dart';
+import 'package:flutterdesigndemo/values/colors_name.dart';
+import 'package:flutterdesigndemo/values/strings_name.dart';
+import 'package:flutterdesigndemo/values/text_styles.dart';
+import 'package:get/get.dart';
 
 class SpecializationDetail extends StatefulWidget {
   const SpecializationDetail({Key? key}) : super(key: key);
@@ -8,8 +20,44 @@ class SpecializationDetail extends StatefulWidget {
 }
 
 class _SpecializationDetailState extends State<SpecializationDetail> {
+  bool isVisible = false;
+  List<BaseApiResponseWithSerializable<SpecializationResponse>>? specializationData = [];
+
+  final apiRepository = getIt.get<ApiRepository>();
+
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  Future<void> initialization() async {
+    setState(() {
+      isVisible = true;
+    });
+    var query = "FIND('${Get.arguments}', ${TableNames.TBL_SPECIALIZATION}, 0)";
+    var data = await apiRepository.getSpecializationDetailApi(query);
+    if (data.records!.isNotEmpty) {
+      specializationData = data.records;
+    } else {
+      Utils.showSnackBar(context, strings_name.str_something_wrong);
+    }
+    setState(() {
+      isVisible = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppWidgets.appBarWithoutBack(strings_name.str_academic_detail),
+      body: Stack(children: [
+        specializationData!.isNotEmpty ? Container() : Container(margin: EdgeInsets.only(top: 100), child: custom_text(text: strings_name.str_no_data, textStyles: centerTextStyleBlack18, alignment: Alignment.center)),
+        Center(
+          child: Visibility(visible: isVisible, child: const CircularProgressIndicator(strokeWidth: 5.0, color: colors_name.colorPrimary)),
+        )
+      ]),
+    ));
   }
 }
