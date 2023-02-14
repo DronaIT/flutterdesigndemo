5e9397a22cefa32faec67861b4bfcb5a281bcf0d
Mansi joshi <joshi mansi>
2023-02-14 23:47:21 +0530

    changes

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/hb/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/hb/AndroidStudioProjects/flutterdesigndemo"
+export "FLUTTER_ROOT=C:\Users\abc\flutter"
+export "FLUTTER_APPLICATION_PATH=G:\DronaApp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
